b0ee4b549164a686c115012ee4bde254934106c8
Vincent Bragard <vincent.bragard@gmail.com>
2024-07-09 15:50:13 +0200

    feat: space and enter keys can close pet and enemy popups

---
+++ modals.js
@@ -0,0 +1,19 @@
+// Meta function guaranteed to run after the DOM is ready
+function onDomReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+onDomReady(function () {
+    document.addEventListener("keydown", function(event) {
+        const key = event.key; // Or const {key} = event; in ES6+
+        if (key === "Enter" || key === " ") {
+            document.getElementById("dailyMessagesDiv").style.display = "none";
+        }
+    });
+});
