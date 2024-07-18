8d02ece272a2256ba35ef5483c857eddce543a2b
MarcElAcoplao <marcrams2007@gmail.com>
2024-07-18 16:52:41 +0200

    new file

---
+++ timedimensions.js
@@ -0,0 +1,8 @@
+//v1.0 feature content
+const Dimensions = [ //Dimension stats
+    {name: "Test", dimensionID: 0, amount: 0, multiplier: 1},
+    {name: "Dimension1", dimensionID: 1, amount: 1, multiplier: 1},
+    {name: "Dimension2", dimensionID: 2, amount: 0, multiplier: 1},
+    {name: "Dimension3", dimensionID: 3, amount: 0, multiplier: 1},
+    {name: "Dimension4", dimensionID: 4, amount: 0, multiplier: 1},
+  ]
